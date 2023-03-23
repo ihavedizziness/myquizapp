b8fcb784004a734a20264aadfadc12653c63c25a
ihavedizziness <almatzhubanovich@gmail.com>
2023-03-23 20:29:47 +0600

    Guess the country from the flag app

--- app/src/main/java/kz/ihavedizziness/myquizapp/ResultActivity.kt
+++ app/src/main/java/kz/ihavedizziness/myquizapp/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
@@ -14,9 +15,12 @@ class ResultActivity : AppCompatActivity() {
         val tvName: TextView = findViewById(R.id.tvName)
         val tvScore: TextView = findViewById(R.id.tvScore)
         val btnFinish: Button = findViewById(R.id.btnFinish)
+        val ivMedal: ImageView = findViewById(R.id.ivMedal)
 
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+
+        ivMedal.setImageResource(R.drawable.ic_medal)
 
         tvName.text = intent.getStringExtra(Constants.USER_NAME)
         tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
